5a03465fe58fa6d902ad576d8a878565e5d8597f
abdolmajidazad <am.azad66@gmail.com>
2016-07-01 16:44:41 +0430

    create puzzel

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -1,7 +1,8 @@
 import {Component} from 'angular2/core'
+import {OnInit} from 'angular2/core'
 @Component({
-  selector : 'my-puzzle',
-  template : `
+  selector: 'my-puzzle',
+  template: `
       <section class="setup"> 
       <h2>Game Setup</h2>
         Enter Your name please : 
@@ -10,29 +11,42 @@ import {Component} from 'angular2/core'
       
       </section>
       <section
-       [ngClass] = "{puzzle : true}"
+       [ngClass] = "{
+          puzzle : true,
+          solved : switch1.value == switch1Number && switch2.value == switch2Number && switch3.value == switch3Number && switch4.value == switch4Number 
+        }"
        [ngStyle] ="{display : name.value === '' ? 'none' : 'block'}"
        > 
-        <h1>The Puzzle</h1> 
-        <p>Ok, welCome <span class="name">XXX</span></p>
+        <h1>
+          The Puzzle | {{switch1.value == switch1Number && switch2.value == switch2Number && switch3.value == switch3Number && switch4.value == switch4Number ? 'SOLVED' : 'NOT SOLVED'}}
+          </h1> 
+        <p>Ok, welCome <span class="name">{{name.value}}</span></p>
         <br>
         Switch 1:
-        <input type="text" #switch1><br>
+        <input type="text" #switch1 (keyup)="0"><br>
         Switch 2:
-        <input type="text" #switch2><br>
+        <input type="text" #switch2 (keyup)="0"><br>
         Switch 3:
-        <input type="text" #switch3><br>
+        <input type="text" #switch3 (keyup)="0"><br>
         Switch 4:
-        <input type="text" #switch4><br>
+        <input type="text" #switch4 (keyup)="0"><br>
       </section>
-      <h2>Congratulations XXX , you did it!</h2>
+      <h2 [hidden] ="switch1.value != switch1Number || switch2.value != switch2Number || switch3.value != switch3Number || switch4.value != switch4Number">Congratulations {{name.value}} , you did it!</h2>
 
   `
 })
-export class PuzzleComponent{
-  switch1Number  : number;
-  switch2Number  : number;
-  switch3Number  : number;
-  switch4Number  : number;
+export class PuzzleComponent implements OnInit {
+  switch1Number:number;
+  switch2Number:number;
+  switch3Number:number;
+  switch4Number:number;
+
+  ngOnInit() {
+    this.switch1Number = Math.round(Math.random());
+    this.switch2Number = Math.round(Math.random());
+    this.switch3Number = Math.round(Math.random());
+    this.switch4Number = Math.round(Math.random());
+    console.log(this.switch1Number, this.switch2Number, this.switch3Number, this.switch4Number)
+  }
 
 }
