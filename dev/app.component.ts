f9ba8d4e7c6f45994cdec4d2f1a809f11dfb7c13
abdolmajidazad <am.azad66@gmail.com>
2016-07-03 23:49:53 +0430

    angular routing

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,13 +1,27 @@
 import {Component} from 'angular2/core';
-import {PuzzleComponent} from './puzzle/puzzle.component'
+// import {PuzzleComponent} from './puzzle/puzzle.component'
+import {RouteConfig, ROUTER_DIRECTIVES} from "angular2/router";
+import {App1Component} from "./app1.component";
+import {App2Component} from "./app2.component";
 
 @Component({
     selector: 'my-app',
     template: `
-        <my-puzzle></my-puzzle>
+        <header>
+          <ul>
+            <li><a [routerLink]="['Component1']">Component1</a></li>
+            <li><a [routerLink]="['Component2']">Component2</a></li>
+          </ul>
+        </header>
+        <router-outlet></router-outlet>
+        
     `,
-    directives : [PuzzleComponent]
+    directives : [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  {path : '/component-1', name:'Component1', component :App1Component,useAsDefault : true},
+  {path : '/component-2', name:'Component2', component :App2Component}
+])
 export class AppComponent {
 
 }
---
+++ dev/app2.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+import {Router} from "angular2/router";
+@Component({
+  template : `
+   <h2>this is component 2 </h2>
+   <button (click) = "onNavigate()">navigate to component 1 </button>
+  `
+})
+export class App2Component{
+  constructor(private _router:Router){}
+  onNavigate (){
+    this._router.navigate(['Component1']);
+  }
+
+}
